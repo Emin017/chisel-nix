eb5eca2878d63d8c3c5eb3b965ac03247715e97e
Qiming Chu <cchuqiming@gmail.com>
2025-02-06 17:50:51 +0800

    [nix] prepare mill-deps before publishLocal
    Signed-off-by: Qiming Chu <cchuqiming@gmail.com>

--- templates/chisel/nix/pkgs/mill-builder.nix
+++ templates/chisel/nix/pkgs/mill-builder.nix
@@ -1,13 +1,23 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
 
-{ stdenvNoCC, mill, rsync, writeText, makeSetupHook, lib }:
+{ add-determinism, stdenvNoCC, mill, rsync, writeText, makeSetupHook, lib }:
 
-{ name, src, millDepModules, millDepsHash, ... }@args:
+{ name, src, millDepModules ? [ ], millDepsHash, preparePhase, publish ? true, ... }@args:
 
 let
   mill-rt-version = lib.head (lib.splitString "+" mill.jre.version);
   cachePrefix = if stdenvNoCC.hostPlatform.isDarwin then "Library/Caches/Coursier" else ".cache/coursier";
+  movePhase =
+    if publish then ''
+      mkdir -p $out/.cache $out/.ivy2
+      mv $TMPDIR/${cachePrefix} $out/.cache/coursier
+      mv $TMPDIR/.ivy2/local $out/.ivy2/local
+    ''
+    else ''
+      mkdir -p $out/.cache $out/.ivy2/local
+      mv $TMPDIR/${cachePrefix} $out/.cache/coursier
+    '';
   self = stdenvNoCC.mkDerivation ({
     name = "${name}-mill-deps";
     inherit src;
@@ -33,17 +43,18 @@ let
       # Use "https://repo1.maven.org/maven2/" only to keep dependencies integrity
       export COURSIER_REPOSITORIES="ivy2Local|central"
 
-      mill -i __.prepareOffline
-      mill -i __.scalaCompilerClasspath
+      ${preparePhase}
+
+      find $TMPDIR -type f -name '*.jar' -exec '${add-determinism}/bin/add-determinism' -j "$NIX_BUILD_CORES" '{}' ';'
+
       runHook postBuild
     '';
 
     installPhase = ''
       runHook preInstall
-      mkdir -p $out/.cache $out/.ivy2
 
-      mv $TMPDIR/${cachePrefix} $out/.cache/coursier
-      mv $TMPDIR/.ivy2/local $out/.ivy2/local
+      ${movePhase}
+
       runHook postInstall
     '';
 
@@ -73,6 +84,13 @@ let
           touch "$tmpdir/.mill/ammonite/rt-${mill-rt-version}.jar"
 
           echo "JAVA HOME dir set to $tmpdir"
+          export MILL_CACHE_PATH=$tmpdir
+        }
+
+        copyMillCache() {
+          mkdir -p $TMPDIR/.cache $TMPDIR/.ivy2
+          cp -r $MILL_CACHE_PATH/.cache/coursier $TMPDIR/.cache/
+          cp -r $MILL_CACHE_PATH/.ivy2/local $TMPDIR/.ivy2/
         }
 
         postUnpackHooks+=(setupMillCache)
